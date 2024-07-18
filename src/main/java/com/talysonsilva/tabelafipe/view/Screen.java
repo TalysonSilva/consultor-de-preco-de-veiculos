8eae2da6a41aa45a596cb606230f874bfbedc026
TalysonSilva <talyson.rodigues@gmail.com>
2024-07-17 21:28:59 -0300

    refactor - Métodos da classe Screen.java

--- src/main/java/com/talysonsilva/tabelafipe/view/Screen.java
+++ src/main/java/com/talysonsilva/tabelafipe/view/Screen.java
@@ -18,60 +18,25 @@ public class Screen {
     private ConvertsData convertsData = new ConvertsData();
     private  final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
+    private String address;
+
     public  void displayMenu(){
         System.out.println("""
+                --------------<CONSULTOR DE PREÇO DE VEICULO>-----------
                 Digite uma das seguintes opção:
                 Carros
                 Motos
                 Caminhão
                 """
         );
+
         String vehicleType = read.nextLine().toLowerCase().strip();
         checkVehicleType(vehicleType);
-
-        //TODO separa em uma função
-        System.out.println("\nModelos dessa marca: ");
-        modeloLista.modelos().stream()
-                .sorted(Comparator.comparing(Data::codigo))
-                .forEach(System.out::println);
-
-        //TODO separa em uma função
-        System.out.println("\nDigite um trecho do nome do veiculo a ser buscado: ");
-        var veiculo = read.nextLine();
-
-        //TODO separa em uma função
-        List<Data> modelosFiltrados = modeloLista.modelos().stream()
-                .filter(a -> a.nome().toLowerCase() .contains(veiculo))
-                .collect(Collectors.toList());
-        //TODO separa em uma função
-        System.out.println("\nModelos filtrados");
-        modelosFiltrados.forEach(System.out::println);
-
-        //TODO separa em uma função
-        System.out.println("\nDigite por favor o código do modelo: ");
-        var codigoModelo = read.nextLine();
-
-        address = address +"/" +codigoModelo+ "/anos";
-        json = api.getAddressData(address);
-        List<Data> anos = convertsData.getJsonList(json, Data.class);
-
-        //TODO separa em uma função
-        List<DetailedVehicleData> veiculos = new ArrayList<>();
-
-        for (int i = 0; i < anos.size() ; i++) {
-            var enderecoAnos = address + "/" + anos.get(i).codigo();
-            json = api.getAddressData(enderecoAnos);
-            DetailedVehicleData veiculoAno = convertsData.getJsonData(json, DetailedVehicleData.class);
-            veiculos.add(veiculoAno);
-        }
-
-        //TODO separa em uma função
-        System.out.println("\nTodos os veiculos filtrados com avaliaçõees por ano: ");
-        veiculos.forEach(System.out::println);
 
     }
 
     private void checkVehicleType(String vehicleType) {
+
         String createdAddress;
         if (vehicleType.contains("carr")){
             createdAddress = URL_BASE + "carros/marcas";
@@ -85,26 +50,96 @@ public class Screen {
     }
 
     private void creatingJson(String address) {
+
         String json = api.getAddressData(address);
-        showVehicleBrands(json);
+        showVehicleBrands(json, address);
+
     }
 
-    private void showVehicleBrands(String json) {
+    private void showVehicleBrands(String json, String address) {
         var listBrands = convertsData.getJsonList(json, Data.class);
         listBrands.stream()
                 .sorted(Comparator.comparing(Data::codigo))
                 .forEach(System.out::println);
 
-        enterVehicleModelCode();
+        enterVehicleModelCode(address);
     }
 
-    private void enterVehicleModelCode() {
+    private void enterVehicleModelCode(String address) {
         System.out.println("\nDigite o codigo do modelo: ");
         String codigoMarca = read.nextLine();
 
-        var address = address + "/" + codigoMarca + "/modelos";
-        var json = api.getAddressData(address);
+        var addressModelos = address + "/" + codigoMarca + "/modelos";
+        var json = api.getAddressData(addressModelos);
         var modeloLista = convertsData.getJsonData(json, VehicleModels.class);
 
+        this.address = addressModelos;
+
+        modelList(modeloLista);
     }
+
+    private void modelList(VehicleModels modeloLista) {
+
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Data::codigo))
+                .forEach(System.out::println);
+
+        var veiculo = enterVehicleName();
+        filterModelVehicleName(veiculo, modeloLista);
+    }
+
+    private String enterVehicleName() {
+        System.out.println("\nDigite um trecho do nome do veiculo a ser buscado: ");
+        var veiculo = read.next();
+
+       return veiculo;
+    }
+
+    private void filterModelVehicleName(String veiculo, VehicleModels modeloLista) {
+        List<Data> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(a -> a.nome().toLowerCase().contains(veiculo))
+                .collect(Collectors.toList());
+
+        System.out.println("\nModelos filtrados");
+        modelosFiltrados.forEach(System.out::println);
+
+        enterVehicleCode(modelosFiltrados);
+    }
+
+    private void enterVehicleCode(List<Data> modelosFiltrados) {
+        System.out.println("\nDigite por favor o código do modelo: ");
+        var codigoModelo = read.next();
+
+        listVehiclesYear(codigoModelo);
+    }
+
+    private void listVehiclesYear(String codigoModelo) {
+
+        address = address +"/" +codigoModelo+ "/anos";
+        var json = api.getAddressData(address);
+        List<Data> anos = convertsData.getJsonList(json, Data.class);
+
+        ListVehicles(anos);
+    }
+
+    private void ListVehicles(List<Data> anos) {
+        List<DetailedVehicleData> veiculos = new ArrayList<>();
+
+        for (int i = 0; i < anos.size() ; i++) {
+            var enderecoAnos = address + "/" + anos.get(i).codigo();
+            var json = api.getAddressData(enderecoAnos);
+            DetailedVehicleData veiculoAno = convertsData.getJsonData(json, DetailedVehicleData.class);
+            veiculos.add(veiculoAno);
+        }
+
+        filterVehicleYear(veiculos);
+    }
+
+    private void filterVehicleYear(List<DetailedVehicleData> veiculos) {
+        System.out.println("\nTodos os veiculos filtrados com avaliaçõees por ano: ");
+        veiculos.forEach(System.out::println);
+    }
+
+
 }
